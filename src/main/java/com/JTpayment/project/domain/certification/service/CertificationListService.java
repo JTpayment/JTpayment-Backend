2def14e4a9939c939d333527871f7e88e9fddd20
hothuuk <s22014@gsm.hs.kr>
2024-01-16 20:10:28 +0900

    :memo: CertificationListService 작성

---
+++ src/main/java/com/JTpayment/project/domain/certification/service/CertificationListService.java
@@ -0,0 +1,8 @@
+package com.JTpayment.project.domain.certification.service;
+
+import com.JTpayment.project.domain.certification.presentation.dto.response.CertificationListResponse;
+
+public interface CertificationListService {
+
+    CertificationListResponse execute();
+}
