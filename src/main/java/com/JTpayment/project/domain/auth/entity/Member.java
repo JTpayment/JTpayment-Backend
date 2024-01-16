0057ac56cb44d78c8d952e171a2bb8a855f5cf9c
오진서 <s22030@gsm.hs.kr>
2024-01-16 17:00:12 +0900

    create :: Member entity

---
+++ src/main/java/com/JTpayment/project/domain/auth/entity/Member.java
@@ -0,0 +1,30 @@
+package com.JTpayment.project.domain.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String loginId;
+
+    @Column(name = "member_password", nullable = false)
+    private String password;
+
+    @Column(name = "member_nickname", nullable = false,unique = true)
+    private String nickName;
+}
