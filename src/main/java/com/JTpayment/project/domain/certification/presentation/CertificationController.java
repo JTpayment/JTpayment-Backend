becfc3be801e821f841bc3bdedd12a1cf007f88a
hothuuk <s22014@gsm.hs.kr>
2024-01-17 19:10:37 +0900

    :memo: request 추가

--- src/main/java/com/JTpayment/project/domain/certification/presentation/CertificationController.java
+++ src/main/java/com/JTpayment/project/domain/certification/presentation/CertificationController.java
@@ -1,13 +1,13 @@
 package com.JTpayment.project.domain.certification.presentation;
 
+import com.JTpayment.project.domain.certification.presentation.dto.request.ApplyRequest;
 import com.JTpayment.project.domain.certification.presentation.dto.response.CertificationListResponse;
+import com.JTpayment.project.domain.certification.service.ApplyRequestService;
 import com.JTpayment.project.domain.certification.service.CertificationListService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,9 +16,17 @@ public class CertificationController {
 
     private final CertificationListService certificationListService;
 
+    private final ApplyRequestService applyRequestService;
+
     @GetMapping
     public ResponseEntity<CertificationListResponse> list() {
         CertificationListResponse certificationListResponse = certificationListService.execute();
         return new ResponseEntity<>(certificationListResponse, HttpStatus.OK);
+    }
+
+    @PostMapping("/request")
+    public ResponseEntity<Void> request(@RequestBody ApplyRequest applyRequest) {
+        applyRequestService.execute(applyRequest);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
