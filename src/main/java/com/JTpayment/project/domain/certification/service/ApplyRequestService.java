46fa19ae1217d86c70860cbb0a865d750ae8a75a
hothuuk <s22014@gsm.hs.kr>
2024-01-17 19:11:29 +0900

    :memo: ApplyRequestService 작성

---
+++ src/main/java/com/JTpayment/project/domain/certification/service/ApplyRequestService.java
@@ -0,0 +1,7 @@
+package com.JTpayment.project.domain.certification.service;
+
+import com.JTpayment.project.domain.certification.presentation.dto.request.ApplyRequest;
+
+public interface ApplyRequestService {
+    void execute(ApplyRequest applyRequest);
+}
