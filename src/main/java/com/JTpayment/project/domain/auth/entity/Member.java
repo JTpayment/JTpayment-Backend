241bc09d3c73c50ba6b596008f5dff7c14190933
오진서 <s22030@gsm.hs.kr>
2024-01-17 16:09:19 +0900

    add :: added Role

--- src/main/java/com/JTpayment/project/domain/auth/entity/Member.java
+++ src/main/java/com/JTpayment/project/domain/auth/entity/Member.java
@@ -1,5 +1,6 @@
 package com.JTpayment.project.domain.auth.entity;
 
+import com.JTpayment.project.domain.auth.entity.enums.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,4 +28,8 @@ public class Member {
 
     @Column(name = "member_nickname", nullable = false,unique = true)
     private String nickName;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
