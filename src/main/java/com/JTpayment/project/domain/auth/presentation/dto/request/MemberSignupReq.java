5bb89c8b18d613705e55f18d46ab5c588ad0721b
오진서 <s22030@gsm.hs.kr>
2024-01-17 16:11:23 +0900

    add :: Role

--- src/main/java/com/JTpayment/project/domain/auth/presentation/dto/request/MemberSignupReq.java
+++ src/main/java/com/JTpayment/project/domain/auth/presentation/dto/request/MemberSignupReq.java
@@ -1,5 +1,6 @@
 package com.JTpayment.project.domain.auth.presentation.dto.request;
 
+import com.JTpayment.project.domain.auth.entity.enums.Role;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,4 +21,6 @@ public class MemberSignupReq {
 
     @NotBlank(message = "공백이거나 중복된 이메일은 사용불가합니다.")
     private String email;
+
+    private Role role;
 }
