b1ffa92fd331259f8c9969b3aed541b075243f13
hothuuk <s22014@gsm.hs.kr>
2024-01-18 20:26:59 +0900

    :memo: BoardCreateRequest 작성

---
+++ src/main/java/com/JTpayment/project/domain/board/presentation/dto/request/BoardCreateRequest.java
@@ -0,0 +1,19 @@
+package com.JTpayment.project.domain.board.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BoardCreateRequest {
+
+    @NotBlank(message = "제목을 입력해주세요.")
+    private String title;
+
+    @NotBlank(message = "내용을 입력해주세요.")
+    private String content;
+}
