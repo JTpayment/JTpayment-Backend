2dc1587974ada664c2d9a68ca378ef88be279c35
hothuuk <s22014@gsm.hs.kr>
2024-01-21 01:17:15 +0900

    :memo: 매개 변수명 변경 및 list 추가

--- src/main/java/com/JTpayment/project/domain/board/presentation/BoardController.java
+++ src/main/java/com/JTpayment/project/domain/board/presentation/BoardController.java
@@ -1,7 +1,9 @@
 package com.JTpayment.project.domain.board.presentation;
 
 import com.JTpayment.project.domain.board.presentation.dto.request.BoardCreateRequest;
+import com.JTpayment.project.domain.board.presentation.dto.response.BoardListResponse;
 import com.JTpayment.project.domain.board.service.BoardCreateService;
+import com.JTpayment.project.domain.board.service.BoardListService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +11,22 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/certification/{certificationId}")
+@RequestMapping("/certification/{cerId}")
 public class BoardController {
 
     private final BoardCreateService boardCreateService;
 
+    private final BoardListService boardListService;
+
     @PostMapping
-    public ResponseEntity<Void> create(@PathVariable Long certificationId, @RequestBody BoardCreateRequest boardCreateRequest) {
-        boardCreateService.execute(certificationId, boardCreateRequest);
+    public ResponseEntity<Void> create(@PathVariable Long cerId, @RequestBody BoardCreateRequest boardCreateRequest) {
+        boardCreateService.execute(cerId, boardCreateRequest);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<BoardListResponse> list(@PathVariable Long cerId) {
+        BoardListResponse boardListResponse = boardListService.execute(cerId);
+        return new ResponseEntity<>(boardListResponse, HttpStatus.OK);
     }
 }
