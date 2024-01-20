0bc1e5e43e8c401ce3ae949a93ab0fe61ebcd6ae
hothuuk <s22014@gsm.hs.kr>
2024-01-21 01:17:28 +0900

    :memo: findByCertification 추가

--- src/main/java/com/JTpayment/project/domain/board/repository/BoardRepository.java
+++ src/main/java/com/JTpayment/project/domain/board/repository/BoardRepository.java
@@ -2,10 +2,14 @@ package com.JTpayment.project.domain.board.repository;
 
 import com.JTpayment.project.domain.auth.entity.Member;
 import com.JTpayment.project.domain.board.entity.Board;
+import com.JTpayment.project.domain.certification.entity.Certification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
     List<Board> findByAuthor(Member member);
+
+    List<Board> findByCertification(Certification certification);
 }
