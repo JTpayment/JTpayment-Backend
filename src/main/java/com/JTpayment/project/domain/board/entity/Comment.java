c85cd4f06c94deadfb969809df054f737d665a76
hothuuk <s22014@gsm.hs.kr>
2024-01-22 01:37:29 +0900

    :memo: CascadeType 삭제

--- src/main/java/com/JTpayment/project/domain/board/entity/Comment.java
+++ src/main/java/com/JTpayment/project/domain/board/entity/Comment.java
@@ -21,7 +21,7 @@ public class Comment {
     @Column(nullable = false)
     private String comment;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member author;
 
