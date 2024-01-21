a0726c353445a9e33522c3d09927718bc3873e96
hothuuk <s22014@gsm.hs.kr>
2024-01-22 01:39:26 +0900

    :memo: CommentRequest 작성

---
+++ src/main/java/com/JTpayment/project/domain/board/presentation/dto/request/CommentRequest.java
@@ -0,0 +1,16 @@
+package com.JTpayment.project.domain.board.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentRequest {
+
+    @NotBlank(message = "댓글을 입력해주세요.")
+    private String comment;
+}
