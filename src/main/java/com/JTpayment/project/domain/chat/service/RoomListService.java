3e904172cb7a1ef0c6e9d4dfba34f8bd41ce7cf8
hothuuk <s22014@gsm.hs.kr>
2024-02-03 18:29:34 +0900

    :memo: RoomListService 작성

---
+++ src/main/java/com/JTpayment/project/domain/chat/service/RoomListService.java
@@ -0,0 +1,8 @@
+package com.JTpayment.project.domain.chat.service;
+
+import com.JTpayment.project.domain.chat.presentation.dto.response.RoomListResponse;
+
+public interface RoomListService {
+
+    RoomListResponse execute();
+}
